e51066d218cc7f5f5ba1efd575bbd10dd3a059d8
Kostiantyn Nahula <kostyannagula@gmail.com>
2024-10-25 21:53:28 +0300

    Created base template fot the employee list page

---
+++ app/employees/constants/employees.ts
@@ -0,0 +1,50 @@
+export const employees = [
+  {
+    name: "Nikita",
+    store: "Smart Fun",
+    email: "mykyta@example.com",
+    role: "sale",
+  },
+  {
+    name: "Aram",
+    store: "Klass Traktora",
+    email: "aram@example.com",
+    role: "sale",
+  },
+  {
+    name: "Eugene",
+    store: "Privoz",
+    email: "eugene@example.com",
+    role: "sale",
+  },
+  {
+    name: "Marina",
+    store: "Klass Rogan",
+    email: "maryna@example.com",
+    role: "sale",
+  },
+  {
+    name: "Sergei",
+    store: "Klass Proletarskaya",
+    email: "sergey@example.com",
+    role: "sale",
+  },
+  {
+    name: "Victoria",
+    store: "Zhykova",
+    email: "victoria@example.com",
+    role: "sale",
+  },
+  {
+    name: "Anastasia",
+    store: "Shift-worker",
+    email: "anastasia@example.com",
+    role: "sale",
+  },
+  {
+    name: "Igor",
+    store: "Shift-worker",
+    email: "igor@example.com",
+    role: "sale",
+  },
+];
