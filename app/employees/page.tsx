d0ad93b6eadcc113cb54c232d1577a5930c9e344
Kostiantyn Nahula <kostyannagula@gmail.com>
2024-10-26 16:45:33 +0300

    Added delete modal to employee page

--- app/employees/page.tsx
+++ app/employees/page.tsx
@@ -3,9 +3,17 @@
 import { useState } from "react";
 import { Sidebar } from "./components/Sidebar";
 import { employees } from "./constants/employees";
+import {
+  Dialog,
+  DialogBackdrop,
+  DialogPanel,
+  DialogTitle,
+} from "@headlessui/react";
+import { ExclamationTriangleIcon } from "@heroicons/react/24/outline";
 
 const Employee = () => {
   const [open, setOpen] = useState<boolean>(false);
+  const [modal, setModal] = useState<boolean>(false);
   return (
     <>
       <div className="px-4 sm:px-6 lg:px-8">
@@ -28,7 +36,16 @@ const Employee = () => {
             </button>
           </div>
         </div>
-        <div className="mt-8 flow-root">
+        <div className="mt-4">
+          <input
+            id="search"
+            name="search"
+            type="text"
+            placeholder="Nikita"
+            className="block rounded-md border-0 py-1.5 pl-1.5 w-1/3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          />
+        </div>
+        <div className="mt-4 flow-root">
           <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
             <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
               <table className="min-w-full divide-y divide-gray-300">
@@ -60,9 +77,9 @@ const Employee = () => {
                     </th>
                     <th
                       scope="col"
-                      className="relative py-3.5 pl-3 pr-4 sm:pr-0"
+                      className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
                     >
-                      <span className="sr-only">Edit</span>
+                      Actions
                     </th>
                   </tr>
                 </thead>
@@ -82,13 +99,20 @@ const Employee = () => {
                         {employee.role}
                       </td>
                       <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
-                        <a
-                          href="#"
-                          className="text-indigo-600 hover:text-indigo-900"
-                          onClick={() => setOpen(true)}
-                        >
-                          Edit
-                        </a>
+                        <div className="flex flex-row gap-3">
+                          <button
+                            className="text-indigo-600 hover:text-indigo-900"
+                            onClick={() => setOpen(true)}
+                          >
+                            Edit
+                          </button>
+                          <button
+                            className="text-indigo-600 hover:text-indigo-900"
+                            onClick={() => setModal(true)}
+                          >
+                            Delete
+                          </button>
+                        </div>
                       </td>
                     </tr>
                   ))}
@@ -99,6 +123,62 @@ const Employee = () => {
         </div>
       </div>
       <Sidebar open={open} setOpen={setOpen} />
+      <Dialog open={modal} onClose={setModal} className="relative z-50">
+        <DialogBackdrop
+          transition
+          className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in"
+        />
+
+        <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+            <DialogPanel
+              transition
+              className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 sm:w-full sm:max-w-lg sm:p-6 data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95"
+            >
+              <div className="sm:flex sm:items-start">
+                <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                  <ExclamationTriangleIcon
+                    aria-hidden="true"
+                    className="h-6 w-6 text-red-600"
+                  />
+                </div>
+                <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                  <DialogTitle
+                    as="h3"
+                    className="text-base font-semibold leading-6 text-gray-900"
+                  >
+                    Delete a user
+                  </DialogTitle>
+                  <div className="mt-2">
+                    <p className="text-sm text-gray-500">
+                      Are you sure you want to delete a user? All of your data
+                      will be permanently removed from our servers forever. This
+                      action cannot be undone.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
+                <button
+                  type="button"
+                  onClick={() => setModal(false)}
+                  className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
+                >
+                  Deactivate
+                </button>
+                <button
+                  type="button"
+                  data-autofocus
+                  onClick={() => setModal(false)}
+                  className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                >
+                  Cancel
+                </button>
+              </div>
+            </DialogPanel>
+          </div>
+        </div>
+      </Dialog>
     </>
   );
 };
